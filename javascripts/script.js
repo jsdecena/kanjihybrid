3d0718bcf3db45c0bb1dcee907942c3c10e76394
Jeff Simons Decena <jeff@jsdecena.me>
2014-10-01 20:40:54 +0800

    updated

--- javascripts/script.js
+++ javascripts/script.js
@@ -2,7 +2,7 @@ jQuery(function(){
 	var value = $("#original").html();
 	var regex = /\s+/gi;
 	var words = value.trim().replace(regex, ' ').split(' ');
-	var wrapped ="";
+	var wrapped = "";
 	for (var i in words) {
 		wrapped = wrapped + "<span style='display:inline-block'> "+words[i]+" </span>&nbsp;";
 	}
