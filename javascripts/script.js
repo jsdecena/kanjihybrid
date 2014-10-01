9edde82a8ebe20f8da7e8fbc6ef1e95aeb1a065c
Jeff Simons Decena <jeff@jsdecena.me>
2014-10-01 20:11:30 +0800

    changed to my index

---
+++ javascripts/script.js
@@ -0,0 +1,11 @@
+jQuery(function(){
+	var value = $("div").html();
+	var regex = /\s+/gi;
+	var words = value.trim().replace(regex, ' ').split(' ');
+	var wrapped ="";
+	for (var i in words) {
+		wrapped = wrapped + "<span style='display:inline-block'> "+words[i]+" </span>&nbsp;";
+	}
+
+	$("#converted").html(wrapped);
+});
